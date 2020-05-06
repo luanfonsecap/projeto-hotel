8f812c1748166e89ed1dfcc3216d06bbb5404e02
luanfonsecap <luanfonsecap@icloud.com>
2020-05-06 19:09:54 -0300

    add new routes do bookingRouter

--- backend/src/routes/bookingRouter.routes.js
+++ backend/src/routes/bookingRouter.routes.js
@@ -3,26 +3,36 @@ import { parseISO } from 'date-fns';
 
 import BookingController from '../controllers/BookingController';
 import Booking from '../models/Booking';
+import User from '../models/User';
+import Hotel from '../models/Hotel';
 
-const bookingController = new BookingController(Booking);
+const bookingController = new BookingController(Booking, User, Hotel);
 
 const bookingRouter = new Router();
 
-bookingRouter.post('/', async (req, res) => {
+bookingRouter.post('/:hotelId', async (req, res) => {
   const {
-    sponsor, contact, hotel, typeRoom, time, guest,
+    initialDate, hotel, finalDate, guest,
   } = req.body;
+  const { userId } = req.headers;
+  const { hotelId } = req.params;
 
-  const formattedTime = {
-    entry: parseISO(time.entry),
-    exit: parseISO(time.exit),
-  };
+  const initalFormattedDate = parseISO(initialDate);
+  const finalFormattedDate = parseISO(finalDate);
 
   const booking = await bookingController.store({
-    sponsor, contact, hotel, typeRoom, time: formattedTime, guest,
+    hotelId, userId, initalFormattedDate, hotel, finalFormattedDate, guest,
   });
 
   return res.status(201).json(booking);
+});
+
+bookingRouter.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const bookings = await bookingController.index(id);
+
+  return res.json(bookings);
 });
 
 export default bookingRouter;
