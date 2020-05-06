316097854a322f2fa67af4b9e3f431414fc888a6
luanfonsecap <luanfonsecap@icloud.com>
2020-05-06 19:08:46 -0300

    add user bookings relation

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,7 +1,12 @@
 import { Schema, model } from 'mongoose';
 
 const UserSchema = new Schema({
+  name: String,
   email: String,
+  bookings: {
+    type: Schema.Types.ObjectId,
+    ref: 'Bookings',
+  },
 });
 
 export default model('User', UserSchema);
