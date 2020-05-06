61efbcb22da1d1d22c90f736b30505d3ad60b581
luanfonsecap <luanfonsecap@icloud.com>
2020-05-06 19:08:36 -0300

    add user validation before log

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -5,14 +5,20 @@ class SessionController {
     this.User = user;
   }
 
-  async store({ email }) {
+  async show(id) {
+    const user = await this.User.findById(id).populate('Bookings');
+
+    return user;
+  }
+
+  async store({ name, email }) {
     let user = await this.User.findOne({ email });
 
     if (user) {
-      throw new AppError(400, 'This user is already logged');
+      throw new AppError(400, 'This user is already registered');
     }
 
-    user = await this.User.create({ email });
+    user = await this.User.create({ name, email });
 
     return user;
   }
