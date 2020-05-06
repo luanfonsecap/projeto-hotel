0b11866060f37233b427f43a84bad0e29d1176cd
luanfonsecap <luanfonsecap@icloud.com>
2020-05-06 19:09:33 -0300

    refactor booking controller

--- backend/src/controllers/BookingController.js
+++ backend/src/controllers/BookingController.js
@@ -1,14 +1,33 @@
+import AppError from '../errors/AppError';
+
 class BookingController {
-  constructor(booking) {
+  constructor(booking, user, hotel) {
     this.Booking = booking;
+    this.User = user;
+    this.Hotel = hotel;
+  }
+
+  async index({ id }) {
+    const bookings = await this.Booking.find({ sponsor: id });
+
+    return bookings;
   }
 
   async store({
-    sponsor, contact, hotel, typeRoom, time, guest,
+    hotelId, userId, initialDate, finalDate, guest,
   }) {
-    const booking = await this.Booking.create({
-      sponsor, contact, hotel, typeRoom, time, guest,
+    this.User.findById(userId).catch(() => {
+      throw new AppError(401, 'User unauthorized.');
     });
+
+    this.Hotel.findById(hotelId).catch(() => {
+      throw new AppError(400, 'Hotel not found with this id.');
+    });
+
+    const booking = await this.Booking.create({
+      sponsor: userId, hotel: hotelId, initialDate, finalDate, guest,
+    });
+    await booking.populate('sponsor').populate('hotel').execPopulate();
 
     return booking;
   }
