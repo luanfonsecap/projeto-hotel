401d06b83fee94b3c654189f4bc673e32802d258
luanfonsecap <luanfonsecap@icloud.com>
2020-05-15 11:51:50 -0300

    add yup validation to booking controller

--- backend/src/controllers/BookingController.js
+++ backend/src/controllers/BookingController.js
@@ -1,3 +1,4 @@
+import * as Yup from 'yup';
 import AppError from '../errors/AppError';
 
 class BookingController {
@@ -22,6 +23,23 @@ class BookingController {
   async store({
     hotelId, userId, initialDate, finalDate, guest,
   }) {
+    const schema = Yup.object().shape({
+      hotelId: Yup.string().required(),
+      userId: Yup.string().required(),
+      initialDate: Yup.object().date().required(),
+      guest: Yup.number().required().min(1),
+    });
+
+    try {
+      schema.validate({
+        hotelId, userId, initialDate, finalDate, guest,
+      });
+    } catch (e) {
+      if (e instanceof Yup.ValidationError) {
+        throw new AppError('Invalid data.');
+      }
+    }
+
     this.User.findById(userId).catch(() => {
       throw new AppError(401, 'User unauthorized.');
     });
