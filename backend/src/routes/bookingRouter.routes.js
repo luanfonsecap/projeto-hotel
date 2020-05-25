3562a314c89db8b22a6409caf13a027b20ee5fa6
luanfonsecap <luanfonsecap@icloud.com>
2020-05-25 20:16:01 -0300

    challenges

--- backend/src/routes/bookingRouter.routes.js
+++ backend/src/routes/bookingRouter.routes.js
@@ -2,13 +2,41 @@ import { Router } from 'express';
 import { parseISO } from 'date-fns';
 
 import BookingController from '../controllers/BookingController';
+import UserController from '../controllers/UserController';
 import Booking from '../models/Booking';
 import User from '../models/User';
 import Hotel from '../models/Hotel';
+import AppError from '../errors/AppError';
 
 const bookingController = new BookingController(Booking, User, Hotel);
+const userController = new UserController(User);
 
 const bookingRouter = new Router();
+
+bookingRouter.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const bookings = await bookingController.index(id);
+
+  return res.json(bookings);
+});
+
+bookingRouter.get('/:id/bookings', async (req, res) => {
+  const { userId } = req.headers;
+  const { id } = req.params;
+
+  const user = await userController.show(userId);
+
+  if (user.profile === 'client') {
+    if (!user._id === id) {
+      throw new AppError(401, 'This bookings it is only available for the owner or admins');
+    }
+  }
+
+  const bookings = await bookingController.show({ id });
+
+  return res.json(bookings);
+});
 
 bookingRouter.post('/:hotelId', async (req, res) => {
   const {
@@ -16,6 +44,12 @@ bookingRouter.post('/:hotelId', async (req, res) => {
   } = req.body;
   const { userId } = req.headers;
   const { hotelId } = req.params;
+
+  const user = await userController.show(userId);
+
+  if (!user.profile === 'client') {
+    throw new AppError(401, "Only client user's can do this operation.");
+  }
 
   const initalFormattedDate = parseISO(initialDate);
   const finalFormattedDate = parseISO(finalDate);
@@ -27,20 +61,20 @@ bookingRouter.post('/:hotelId', async (req, res) => {
   return res.status(201).json(booking);
 });
 
-bookingRouter.get('/:id', async (req, res) => {
+bookingRouter.delete('/:id/booking', async (req, res) => {
+  const { userId } = req.headers;
   const { id } = req.params;
 
-  const bookings = await bookingController.index(id);
+  const user = await userController.show(userId);
 
-  return res.json(bookings);
+  if (!user.profile === 'admin') {
+    throw new AppError(401, "Only admin user's can do this operation.");
+  }
+
+  await bookingController.destroy(id);
+
+  return res.status(204).send();
 });
 
-bookingRouter.get('/:id/bookings', async (req, res) => {
-  const { id } = req.params;
-
-  const bookings = await bookingController.show({ id });
-
-  return res.json(bookings);
-});
 
 export default bookingRouter;
--- backend/src/routes/hotelsRouter.routes.js
+++ backend/src/routes/hotelsRouter.routes.js
@@ -4,9 +4,13 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import HotelController from '../controllers/HotelController';
+import UserController from '../controllers/UserController';
 import Hotel from '../models/Hotel';
+import User from '../models/User';
+import AppError from '../errors/AppError';
 
 const hotelController = new HotelController(Hotel);
+const userController = new UserController(User);
 
 const hotelsRouter = new Router();
 
@@ -27,10 +31,17 @@ hotelsRouter.get('/:name', async (req, res) => {
 });
 
 hotelsRouter.post('/', upload.single('image'), async (req, res) => {
+  const { id } = req.headers;
   const {
     name, uf, city, street, quantity, dailyValue,
   } = req.body;
   const { image } = req.file;
+
+  const user = await userController.show(id);
+
+  if (!user.profile === 'admin') {
+    throw new AppError(401, "Only admin user's can do this operation.");
+  }
 
   const hotel = await hotelController.store({
     name, uf, city, street, quantity, dailyValue, image,
@@ -44,6 +55,12 @@ hotelsRouter.put('/:id', async (req, res) => {
   const {
     name, uf, city, street, quantityRoom, dailyValue,
   } = req.body;
+
+  const user = await userController.show(id);
+
+  if (!user.profile === 'admin') {
+    throw new AppError(401, "Only admin user's can do this operation.");
+  }
 
   const hotel = await hotelController.update({
     id,
@@ -60,6 +77,12 @@ hotelsRouter.put('/:id', async (req, res) => {
 
 hotelsRouter.delete('/:id', async (req, res) => {
   const { id } = req.params;
+
+  const user = await userController.show(id);
+
+  if (!user.profile === 'admin') {
+    throw new AppError(401, "Only admin user's can do this operation.");
+  }
 
   await hotelController.delete({ id });
 
